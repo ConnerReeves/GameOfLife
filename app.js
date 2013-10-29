cf48bbfeb835f897770295b475a566153a9ff314
ConnerReeves <connerreeves@gmail.com>
2013-10-29 09:10:10 -0700

    Added command line arguments to app.js

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-var gameBoard = require('./gameBoard');
+var _         = require('lodash'),
+	gameBoard = require('./gameBoard');
 
-console.log('Initial Board:');
-gameBoard.loadFromFile('rallyExampleBoard.txt').display();
+//Process command line arguments
+if (process.argv.length < 3) {
+	throw new Error('Use "node app [fileName.txt] [generationsToProcess (optional)]');
+} else {
+	//Load the gameboard from the provided file name and display the initial version
+	gameBoard.loadFromFile(process.argv[2]);
+	console.log('Initial Game Board:');
+	gameBoard.display();
 
-var counter = 0;
-setInterval(function() {
-	console.log('Generation ' + ++counter + ':');
-	gameBoard.proceedGeneration().display();
-	if (counter > 6) clearInterval(this);
-}, 1000);
+	//Process the provided number of generations, or a single generation by default
+	var generationsToProcess = (process.argv.length > 3) ? parseInt(process.argv[3], 10) || 1 : 1;
+	_.each(_.range(1,generationsToProcess + 1), function(generationNumber) {
+		//Display the board after each new generation is processed
+		console.log('Generation ' + generationNumber + ':');
+		gameBoard.proceedGeneration().display();
+	});
+}
